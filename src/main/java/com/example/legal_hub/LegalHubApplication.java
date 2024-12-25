57f233cb5c524e250577faab80ef7cf2b63ea976
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-25 12:31:04 +0000

    Initial setup

---
+++ src/main/java/com/example/legal_hub/LegalHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.legal_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/legal_hub/LegalHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.legal_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
