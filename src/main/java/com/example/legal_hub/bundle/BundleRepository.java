87384a5cdc20d234f684a2b6598b31f6f76c62bb
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-31 20:41:04 +0000

    Code update: Initial setup

---
+++ src/main/java/com/example/legal_hub/bundle/BundleRepository.java
@@ -0,0 +1,7 @@
+package com.example.legal_hub.bundle;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BundleRepository extends CrudRepository<Bundle, String> {
+
+}
