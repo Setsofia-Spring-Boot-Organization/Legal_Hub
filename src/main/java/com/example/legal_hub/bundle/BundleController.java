901d973d2cc008e1a0f0fb6fbd285f20f9d42045
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2024-12-31 20:41:54 +0000

    Code update: Initial setup

---
+++ src/main/java/com/example/legal_hub/bundle/BundleController.java
@@ -0,0 +1,26 @@
+package com.example.legal_hub.bundle;
+
+import com.example.legal_hub.common.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(path = "legal_hub/api/v1/bundles")
+public class BundleController {
+
+    private final BundleService bundleService;
+
+    public BundleController(BundleService bundleService) {
+        this.bundleService = bundleService;
+    }
+
+    @PostMapping(path = "/upload")
+    public ResponseEntity<Response<?>> uploadBundle(
+            @RequestPart(name = "file") MultipartFile file
+    ) throws IOException {
+        return bundleService.uploadBundle(file);
+    }
+}
