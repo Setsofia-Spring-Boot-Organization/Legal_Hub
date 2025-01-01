9a5d0af10670a9faf23f44d06a39dc3d037ec971
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-01-01 09:53:17 +0000

    Code update: Initial setup

--- src/main/java/com/example/legal_hub/bundle/BundleService.java
+++ src/main/java/com/example/legal_hub/bundle/BundleService.java
@@ -1,5 +1,6 @@
 package com.example.legal_hub.bundle;
 
+import com.example.legal_hub.bundle.requests.UploadNewBundle;
 import com.example.legal_hub.common.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,5 +16,5 @@ public interface BundleService {
      * @return a {@link ResponseEntity} containing a {@link Response} with the status and details of the upload operation
      * @throws IOException if an error occurs while processing the file
      */
-    ResponseEntity<Response<?>> uploadBundle(MultipartFile file) throws IOException;
+    ResponseEntity<Response<?>> uploadBundle(MultipartFile file, UploadNewBundle newBundle) throws IOException;
 }
