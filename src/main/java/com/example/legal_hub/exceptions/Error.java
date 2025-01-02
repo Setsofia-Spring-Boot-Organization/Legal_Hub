0ce473992c43705c3aa15f41f7e8f8cb3928fd58
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-01-02 08:58:02 +0000

    Code update: Initial setup

--- src/main/java/com/example/legal_hub/exceptions/Error.java
+++ src/main/java/com/example/legal_hub/exceptions/Error.java
@@ -3,7 +3,8 @@ package com.example.legal_hub.exceptions;
 public enum Error {
 
     ERROR_SAVING_DATA("an error occur while saving data"),
-    INVALID_BUNDLE_ID("the submitted bundle id is invalid");
+    INVALID_BUNDLE_ID("the submitted bundle id is invalid"),
+    ERROR_UPDATING_DATA("an error occur while updating data");
 
     public final String label;
     Error(String label) {
