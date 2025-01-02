990195f6e365f02d3106f7d99e17d42d550f2e36
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-01-02 14:59:33 +0000

    Code update: Initial setup

---
+++ src/main/java/com/example/legal_hub/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.example.legal_hub.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Value("${deployed-backend-url}")
+    private String DEPLOYED_FRONT_END_URL;
+    @Value("${local-backend-url}")
+    private String LOCAL_FRONT_END_URL;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of(LOCAL_FRONT_END_URL, DEPLOYED_FRONT_END_URL)); // Allow all origins with patterns
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.addExposedHeader("Authorization");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
