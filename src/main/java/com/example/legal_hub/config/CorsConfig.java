61c2520ca535d4e7040e28228b731ae3364fe7d1
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-01-02 16:11:41 +0000

    Code update: Initial setup

--- src/main/java/com/example/legal_hub/config/CorsConfig.java
+++ src/main/java/com/example/legal_hub/config/CorsConfig.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
@@ -19,15 +19,16 @@ public class CorsConfig implements WebMvcConfigurer {
     private String LOCAL_FRONT_END_URL;
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsFilter corsFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of(LOCAL_FRONT_END_URL, DEPLOYED_FRONT_END_URL)); // Allow all origins with patterns
+        configuration.setAllowedOriginPatterns(List.of(DEPLOYED_FRONT_END_URL, LOCAL_FRONT_END_URL)); // Allow all origins
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.addExposedHeader("Authorization");
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        source.registerCorsConfiguration("/", configuration);
+        return new CorsFilter(source);
     }
 }
