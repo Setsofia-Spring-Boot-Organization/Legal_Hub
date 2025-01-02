ff48a56ecebbe95108ce6a066e9b09c75a8f7f71
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-01-02 15:13:56 +0000

    Code update: Initial setup

--- src/main/java/com/example/legal_hub/config/CorsConfig.java
+++ src/main/java/com/example/legal_hub/config/CorsConfig.java
@@ -1,32 +1,21 @@
 package com.example.legal_hub.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
     @Value("${deployed-backend-url}")
     private String DEPLOYED_FRONT_END_URL;
     @Value("${local-backend-url}")
     private String LOCAL_FRONT_END_URL;
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of(LOCAL_FRONT_END_URL, DEPLOYED_FRONT_END_URL)); // Allow all origins with patterns
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.addExposedHeader("Authorization");
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("api/v1/**")
+                .allowedOrigins(DEPLOYED_FRONT_END_URL, LOCAL_FRONT_END_URL)
+                .allowedMethods("GET", "POST", "DELETE", "PATCH", "UPDATE", "OPTIONS");
     }
 }
