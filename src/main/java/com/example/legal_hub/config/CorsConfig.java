76b00ac5e336bb84418dc289728ca6f1a4a33525
SETSOFIA NUSETOR <nusetorsetsofia101@gmail.com>
2025-01-03 09:57:31 +0000

    Code update: Initial setup

--- src/main/java/com/example/legal_hub/config/CorsConfig.java
+++ src/main/java/com/example/legal_hub/config/CorsConfig.java
@@ -1,34 +1,22 @@
 package com.example.legal_hub.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Value("${deployed-frontend-url}")
-    private String DEPLOYED_FRONT_END_URL;
+    private String DEPLOYED_FRONTEND_URL;
     @Value("${local-frontend-url}")
-    private String LOCAL_FRONT_END_URL;
+    private String LOCAL_FRONTEND_URL;
 
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of(DEPLOYED_FRONT_END_URL, LOCAL_FRONT_END_URL)); // Allow all origins
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.addExposedHeader("Authorization");
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/", configuration);
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/api/v1/**")
+                .allowedOrigins(LOCAL_FRONTEND_URL+"/", DEPLOYED_FRONTEND_URL+"/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH");
     }
 }
